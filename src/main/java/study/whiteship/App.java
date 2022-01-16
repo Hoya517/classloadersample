033fe618b2cbf2fbb50c29226d0d0e60258dc157
Kim Juho <sv05159@naver.com>
2022-01-16 23:59:52 +0900

    클래스 로더

--- src/main/java/study/whiteship/App.java
+++ src/main/java/study/whiteship/App.java
@@ -4,7 +4,19 @@ package study.whiteship;
  * Hello world!
  */
 public class App {
+
+    Book book = new Book();
+
+//    static String name = "juho";
+    static String name;
+    static {
+        name = "juho";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ClassLoader classLoader = App.class.getClassLoader();
+        System.out.println(classLoader);
+        System.out.println(classLoader.getParent());
+        System.out.println(classLoader.getParent().getParent());
     }
 }
