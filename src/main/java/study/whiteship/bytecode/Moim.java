c8c8605939e74ed901ba593b33a4b14b23a2cdff
Kim Juho <sv05159@naver.com>
2022-01-17 22:43:58 +0900

    코드 커버리지는 어떻게 측정할까?
    - 예제 소스

---
+++ src/main/java/study/whiteship/bytecode/Moim.java
@@ -0,0 +1,20 @@
+package study.whiteship.bytecode;
+
+public class Moim {
+
+    int maxNumberOfAttendees;  //최대 참가자
+    int numberOfEnrollment;  //신청수
+
+    //신청이 꽉 찼는지 확인하는 메서드
+    public boolean isEnrollmentFull() {
+        if (maxNumberOfAttendees == 0) {
+            return false;
+        }
+
+        if (numberOfEnrollment < maxNumberOfAttendees) {
+            return false;
+        }
+
+        return true;
+    }
+}
